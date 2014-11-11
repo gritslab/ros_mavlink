39f5ae9f6e979768a9a759c7771241b95dfb100d
warara <urs.borrmann@tum.de>
2014-11-11 13:26:11 -0500

    Added launch file capability

--- src/ros_mavlink_main.cpp
+++ src/ros_mavlink_main.cpp
@@ -5,13 +5,26 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "ros_mavlink");
 
-    string port = "/dev/ttyUSB0";
-    int baud = 115200;
-    int sysid = 42;
-    int compid = 110;
-    RosMavlink rm(port, baud, sysid, compid);
+    std::string port;
+    const std::string PATH_DEFAULT = "/dev/ttyO1";
+    const int BAUD_DEFAULT =115200;
+    const int COMPID_DEFAULT = 110;
+    const int SYSID_DEFAULT = 42;
+    int baud;
+    int sysid;
+    int compid;
 
     ros::NodeHandle ros_nh;
+
+    // read all necessary parameters provided in launch file, or use standard values, if not provided or started via rosrun
+    ros_nh.param("ros_mavlink/uart_port", port, PATH_DEFAULT);
+    ros_nh.param("ros_mavlink/uart_baudrate", baud, BAUD_DEFAULT);
+    ros_nh.param("ros_mavlink/uart_compid", compid, COMPID_DEFAULT);
+    ros_nh.param("ros_mavlink/uart_sysid", sysid, SYSID_DEFAULT);
+
+    RosMavlink rm(port, baud, sysid, compid);
+
+
 
     ros::Rate loop_rate(10);
 
